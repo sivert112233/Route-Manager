9e47f119f40bdb4b97e25215155bdb5e04055908
Sivert <sivert112233@gmail.com>
2026-01-18 16:26:30 +0100

    10 jan kl 16:26

---
+++ data.js
@@ -0,0 +1,89 @@
+export const routedata = {
+  valdres: [
+    {
+      loc: 'Dokka',
+      des: 'Nordre-Land bibliotek',
+      stopNum: 1
+    },
+    {
+      loc: 'Dokka',
+      des: 'Post i butikk (Ream 1000)',
+      stopNum: 2
+    },
+    {
+      loc: 'Dokka',
+      des: 'Dokka politistasjon',
+      stopNum: 3
+    },
+    {
+      loc: 'Etnedal',
+      des: 'Etnedal bibliotek',
+      stopNum: 4
+    },
+    {
+      loc: 'Bagn',
+      des: 'Sør-Aurdal bibliotek',
+      stopNum: 5
+    },
+    {
+      loc: 'Leira i Valdres',
+      des: 'Valdres VGS biblioteket',
+      stopNum: 6
+    },
+    {
+      loc: 'Fagernes',
+      des: 'Sør-Aurdal bibliotek',
+      stopNum: 7
+    },
+    {
+      loc: 'Fagernes',
+      des: 'Post i butikk (Eurospar)',
+      stopNum: 8
+    },
+    {
+      loc: 'Fagernes',
+      des: 'Fagernes politistasjon',
+      stopNum: 9
+    },
+    {
+      loc: 'Fagernes',
+      des: 'Fagernes politistasjon',
+      stopNum: 10
+    },
+    {
+      loc: 'Slidre',
+      des: 'Vestre-slidre bibliotek',
+      stopNum: 11
+    },
+    {
+      loc: 'Ryfoss',
+      des: 'Bøker til Vang (Joker)',
+      stopNum: 12
+    },
+    {
+      loc: 'Vang i Valdres',
+      des: 'Vang bibliotek',
+      stopNum: 13
+    },
+    {
+      loc: 'Heggenes',
+      des: 'Øystre-slidre bibliotek',
+      stopNum: 14
+    },
+    {
+      loc: 'Gjøvik',
+      des: 'NTNU Gjøvik',
+      stopNum: 15
+    },
+    {
+      loc: 'Gjøvik',
+      des: 'Gjøvik bibliotek',
+      stopNum: 16
+    },
+    {
+      loc: 'Brumunddal',
+      des: 'Sign On',
+      stopNum: 17
+    },
+  ]
+}
